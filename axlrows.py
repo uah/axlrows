cb2a790ce3075369c97b8b6e79427870907bc59f
Hunter Fuller <hf0002@uah.edu>
2017-07-25 17:43:55 -0500

    Convert everything to an object, and add phone get/update/apply methods

--- axlrows.py
+++ axlrows.py
@@ -1,27 +1,63 @@
 from suds.client import Client
 import json
+from ptpython import repl
 
-print("Loading configuration")
-with open('config.json', 'r') as file:
-    config = json.loads(file.read())
+class CiscoUCM:
+    def __init__(self):
+        print("Loading configuration")
+        with open('config.json', 'r') as file:
+            config = json.loads(file.read())
+            print("Bringing up client")
+            self.__client = Client(config['wsdl'], location=config['location'], username=config['username'], password=config['password'])
 
-print("Bringing up client")
-client = Client(config['wsdl'],location=config['location'], username=config['username'], password=config['password'])
+    def get_phone(self, name):
+        try:
+            phones = self.__client.service.listPhone({'name':name})[0][0]
+        except IndexError:
+            return None
+        uuid = phones[0]._uuid
+        return self.__client.service.getPhone(uuid=uuid)[0][0]
+    
+    def update_phone(self, phone, **kwargs):
+        self.__client.service.updatePhone(uuid=phone._uuid, **kwargs)
 
-print("Querying")
-phone = client.service.getPhone(uuid=client.service.listPhone({'name':'SEP6C416A3693DE'})[0][0][0]._uuid)[0][0]
+    def apply_phone(self, phone):
+        self.__client.service.applyPhone(uuid=phone._uuid)
 
-lines = phone.lines[0]
-print(lines)
+    def get_lines(self, dn):
+        try:
+            lines = uuid=self.__client.service.listLine({'pattern':dn})[0][0]
+        except IndexError:
+            return None
+        uuids = [line._uuid for line in lines]
+        return [self.__client.service.getLine(uuid=x)[0][0] for x in uuids]
+        #return self.__client.service.getLine(uuid=uuid)[0][0]
 
-dns = [line.dirn.pattern for line in lines]
-print("DNs on this phone: ")
-print(dns)
 
-for dn in dns:
-    line = client.service.getLine(uuid=client.service.listLine({'pattern':dn})[0][0][0]._uuid)[0][0]
-    print("DN " + dn + " appears on:")
-    print(line.associatedDevices[0])
+if __name__ == "__main__":
+    print("Making ucm object")
+    ucm = CiscoUCM()
 
-print("Shutting down")
+    print("Starting repl")
+    #repl.embed(globals(), locals())
+    method_names = [m for m in dir(ucm) if '__' not in m]
+    repl.embed(dict(zip(method_names, [getattr(ucm, mn) for mn in method_names])), locals())
+
+    quit()
+
+    print("Querying")
+    phone = ucm.get_phone('SEPBC671C30B2F7')
+
+    lines = phone.lines[0]
+    print(lines)
+
+    dns = [line.dirn.pattern for line in lines]
+    print("DNs on this phone: ")
+    print(dns)
+
+    for dn in dns:
+        line = ucm.get_line(dn)
+        print("DN " + dn + " appears on:")
+        print(line.associatedDevices[0])
+
 
